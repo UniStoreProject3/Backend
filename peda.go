d2395ceda2494f191e9e8386c6e7a30a745c3f32
Befous <befousmain@gmail.com>
2023-11-06 22:24:51 +0700

    Update peda.go : Menghapus function tidak dipakai dan memodifikasi function

--- peda.go
+++ peda.go
@@ -27,7 +27,11 @@ func MembuatUser(mongoenv, dbname, collname string, r *http.Request) string {
 		if hashErr != nil {
 			response.Message = "Gagal Hash Password" + err.Error()
 		}
-		InsertUserdata(mconn, collname, datauser.Username, datauser.Role, hash)
+		hashRole, hashErrRole := HashRole(datauser.Role)
+		if hashErrRole != nil {
+			response.Message = "Gagal Hash Role" + err.Error()
+		}
+		InsertUserdata(mconn, collname, datauser.Username, hash, hashRole)
 		response.Message = "Berhasil Input data"
 	}
 	return ReturnStruct(response)
@@ -48,14 +52,17 @@ func MembuatTokenUser(privatekey, mongoenv, dbname, collname string, r *http.Req
 			if err != nil {
 				response.Message = "Gagal Encode Token : " + err.Error()
 			} else {
-				response.Message = "Selamat Datang"
-				response.Token = tokenstring
+				if IsRoleValid(mconn, collname, datauser) {
+					response.Message = "Selamat Datang"
+					response.Token = tokenstring
+				} else {
+					response.Message = "Akun anda tidak memiliki role"
+				}
 			}
 		} else {
 			response.Message = "Password Salah"
 		}
 	}
-
 	return ReturnStruct(response)
 }
 
@@ -70,70 +77,6 @@ func HapusUser(mongoenv, dbname, collname string, r *http.Request) string {
 	} else {
 		DeleteUser(mconn, collname, datauser)
 		response.Message = "Berhasil Delete data"
-	}
-	return ReturnStruct(response)
-}
-
-func LoginDenganRole(privatekey, mongoenv, dbname, collname string, r *http.Request) string {
-	var response Credential
-	response.Status = false
-	mconn := SetConnection(mongoenv, dbname)
-	var datauser User
-	err := json.NewDecoder(r.Body).Decode(&datauser)
-	if err != nil {
-		response.Message = "error parsing application/json: " + err.Error()
-	} else {
-		if IsPasswordValid(mconn, collname, datauser) {
-			response.Status = true
-			tokenstring, err := watoken.Encode(datauser.Username, os.Getenv(privatekey))
-			if err != nil {
-				response.Message = "Gagal Encode Token : " + err.Error()
-			} else {
-				if CekRole(mconn, collname, datauser) {
-					response.Message = "Selamat Datang Admin"
-					response.Token = tokenstring
-				} else {
-					response.Message = "Selamat Datang User"
-					response.Token = tokenstring
-				}
-			}
-		} else {
-			response.Message = "Password Salah"
-		}
-	}
-	return ReturnStruct(response)
-}
-
-func LoginDenganRole2(privatekey, mongoenv, dbname, collname string, r *http.Request) string {
-	var response Credential
-	response.Status = false
-	mconn := SetConnection(mongoenv, dbname)
-	var datauser User
-	err := json.NewDecoder(r.Body).Decode(&datauser)
-	if err != nil {
-		response.Message = "error parsing application/json: " + err.Error()
-	} else {
-		if IsPasswordValid(mconn, collname, datauser) {
-			response.Status = true
-			tokenstring, err := watoken.Encode(datauser.Username, os.Getenv(privatekey))
-			if err != nil {
-				response.Message = "Gagal Encode Token : " + err.Error()
-			} else {
-				if CekRoleAdmin(mconn, collname, datauser) {
-					response.Message = "Selamat Datang Admin"
-					response.Token = tokenstring
-				}
-				if CekRoleUser(mconn, collname, datauser) {
-					response.Message = "Selamat Datang User"
-					response.Token = tokenstring
-				} else {
-					response.Message = "Anda tidak memiliki role"
-					response.Token = tokenstring
-				}
-			}
-		} else {
-			response.Message = "Password Salah"
-		}
 	}
 	return ReturnStruct(response)
 }
