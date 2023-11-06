ad9ce4f3d9599726bdd92e7370873d60e416c8b1
Befous <befousmain@gmail.com>
2023-11-06 22:24:21 +0700

    Update hash.go : Menghapus function tidak dipakai dan memodifikasi function

--- hash.go
+++ hash.go
@@ -11,3 +11,13 @@ func CheckPasswordHash(password, hash string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err == nil
 }
+
+func HashRole(role string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(role), 14)
+	return string(bytes), err
+}
+
+func CheckRoleHash(role, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(role))
+	return err == nil
+}
